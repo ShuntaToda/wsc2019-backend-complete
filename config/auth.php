7efadfc57a2e6958baccd57955485ec0b0df58f8
ShuntaToda <todashuntawork@gmail.com>
2024-05-27 19:40:41 +0900

    Attendeeを認証用モデルに指定

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'attendees',
+        ],
     ],
 
     /*
@@ -63,6 +67,10 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\Models\User::class,
+        ],
+        'attendees' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Attendee::class,
         ],
 
         // 'users' => [

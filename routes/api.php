987e493527e9826bb00cdc5214b7904a4c2f35f9
ShuntaToda <todashuntawork@gmail.com>
2024-05-28 16:36:57 +0900

    logoutのルートを定義

--- routes/api.php
+++ routes/api.php
@@ -17,12 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix("v1")->group(function () {
-    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-
     Route::post('login', [LoginController::class, "login"]);
-    Route::post('logout', [LoginController::class, "logout"]);
+
+    Route::middleware("auth:sanctum")->group(function () {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+        Route::post('logout', [LoginController::class, "logout"]);
+    });
 
     Route::get('events', [EventController::class, "index"]);
 

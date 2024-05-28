93155810659744a7917ab57be3e358b402bb311a
ShuntaToda <todashuntawork@gmail.com>
2024-05-28 14:44:41 +0900

    ログイン機能追加

--- app/Http/Controllers/api/LoginController.php
+++ app/Http/Controllers/api/LoginController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Attendee;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    public function login(Request $request)
+    {
+        $attendee = Attendee::where("lastname", $request->lastname)->first();
+        if ($attendee !== null && $attendee->registration_code === $request->registration_code) {
+            return $attendee->createToken($request->lastname)->plainTextToken;
+        }
+
+        return response()->json(["message" => "Invalid login"], 401);
+    }
 }

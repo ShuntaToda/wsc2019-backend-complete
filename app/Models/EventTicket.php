83d1bb45a4b96565f5cc201a881496671a01689d
ShuntaToda <todashuntawork@gmail.com>
2024-05-29 16:28:22 +0900

    isAvailableメソッドを実装

--- app/Models/EventTicket.php
+++ app/Models/EventTicket.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class EventTicket extends Model
 {
     use HasFactory;
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class, "ticket_id");
+    }
+
+    public function isAvailable()
+    {
+        $special_validity = json_decode($this->special_validity);
+        switch ($special_validity->type) {
+            case ("amount"):
+                $ticket_count = $this->registrations()->count();
+                return $ticket_count <= $special_validity->amount;
+            case ("date"):
+                $target_date = $special_validity->date;
+                $current_data = date("Y-m-d");
+                return $target_date >= $current_data;
+            default:
+                return true;
+        };
+    }
 }

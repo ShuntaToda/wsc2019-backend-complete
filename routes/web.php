f912e96eee630a9c83fa83a9a60a5b846b1dbe5b
ShuntaToda <todashuntawork@gmail.com>
2024-05-31 15:51:41 +0900

    logoutのルート設定￥

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', [LoginController::class, "index"])->name("login");
 Route::post('/login', [LoginController::class, "login"])->name("login");
+Route::get('/logout', [LoginController::class, "logout"])->name("logout");
 
 Route::middleware("auth")->get("/", function () {
     return redirect(route("admin.event.index"));

f37e931722a785fb36ba751f7d739f684fba8424
ShuntaToda <todashuntawork@gmail.com>
2024-06-03 16:04:16 +0900

    create ticketのルート定義

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,5 +39,10 @@ Route::middleware("auth")->prefix("admin")->as("admin.")->group(function () {
         Route::get("index", function () {
             return view("reports.index");
         })->name("index");
+    });
+
+    Route::prefix("ticket")->as("ticket.")->group(function () {
+        Route::get("create", [TicketController::class, "create"])->name("create");
+        Route::post("create", [TicketController::class, "store"])->name("create");
     });
 });
